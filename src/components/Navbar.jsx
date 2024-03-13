8a3803ea447b075de73a76fd957c63be8f9c66b4
WaliMuhammadAhmad <2021se39student.uet.edu.pk>
2024-03-13 17:33:16 +0500

    supreme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,8 @@ function Navbar() {
   return (
     <div className='fixed z-[999] w-full px-[2vw] py-[3vh] font-["Neue_Montreal"] flex justify-between align-center backdrop-blur-sm'>
       <div className="logo">
-        <a className='invert flex' href="#">
-          <img src="img\logo\suprem.svg" alt="" />
-          <img src="img\logo\E.svg" alt="" />
+        <a className='invert flex w-[10vw]' href="#">
+          <img src="img\logo\supreme.svg" alt="" />
         </a>
       </div>
       <div className="links flex gap-10 justify-center items center">
